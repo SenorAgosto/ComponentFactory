d80fa4c264e63945bb60fcad89c10688c556e68c
Austin Gilbert <ceretullis@gmail.com>
2019-10-27 14:39:49 -0500

    code: eliminating all code, we're going to do this clean-slate and try to get this to be a compile-time construct

--- component_factory/exceptions.cpp
+++
@@ -1,9 +0,0 @@
-#include <component_factory/exceptions.hpp>
-
-namespace component_factory {
-
-    ComponentDoesntExist::ComponentDoesntExist(const std::string& componentName)
-        : std::runtime_error("Component '" + componentName + "' doesn't exist in ComponentFactory.")
-    {
-    }
-}
